ae908b911976be14c377f260f7a45021a43d91c0
Matthias Osthues <osthues.matthias@gmail.com>
2020-12-10 14:33:16 +0100

    add boilerplate header to files

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+# Copyright 2020 The KubeLB Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 set -o errexit
 set -o nounset
@@ -23,7 +37,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 bash "${CODEGEN_PKG}"/generate-groups.sh "client,lister,informer" \
   ${MODULE}/${OUTPUT_PKG} ${MODULE}/${APIS_PKG} \
   ${GROUP_VERSION} \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt \
+  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate/boilerplate.go.txt \
   --output-base "$(dirname "${BASH_SOURCE[0]}")"
 
 
