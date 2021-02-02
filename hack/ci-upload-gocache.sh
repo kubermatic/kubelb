a262806692c5e8abf12f449261fb1ed3ffc46d60
Matthias Osthues <osthues.matthias@gmail.com>
2021-02-02 20:20:40 +0100

    fix gocache boilerplate

--- hack/ci-upload-gocache.sh
+++ hack/ci-upload-gocache.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2021 The Kueblb Authors.
+# Copyright 2020 The Kueblb Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
