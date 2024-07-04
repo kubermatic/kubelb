996dce94125b2e49a5e8e0e8ae0758b732743aaf
Waleed Malik <ahmedwaleedmalik@gmail.com>
2024-07-04 15:56:05 +0500

    TEST
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- hack/ci/e2e/tests.sh
+++ hack/ci/e2e/tests.sh
@@ -18,5 +18,7 @@ set -euo pipefail
 
 source "${ROOT_DIR}/hack/lib.sh"
 
+sleep 50000
+
 echodate "Running tests"
 KUBECONFIGS_DIR="${TMPDIR}" go test -timeout 30m -v "${ROOT_DIR}/hack/ci/e2e/tests/" -coverprofile cover.out
