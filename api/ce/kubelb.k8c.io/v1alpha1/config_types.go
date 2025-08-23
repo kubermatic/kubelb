64edb605e21185e69a582acb6fef33b7e4a93981
Waleed Malik <ahmedwaleedmalik@gmail.com>
2025-08-23 22:33:59 +0500

    By default, skip adding DNS and Certs annotations for tunnels
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- api/ce/kubelb.k8c.io/v1alpha1/config_types.go
+++ api/ce/kubelb.k8c.io/v1alpha1/config_types.go
@@ -54,6 +54,16 @@ type ConfigDNSSettings struct {
 	// This is only used when LoadBalancer.Spec.Hostname is set.
 	// +optional
 	AllowExplicitHostnames bool `json:"allowExplicitHostnames,omitempty"`
+
+	// UseDNSAnnotations is a flag that can be used to add DNS annotations to DNS resources.
+	// This is only used when LoadBalancer.Spec.Hostname is set.
+	// +optional
+	UseDNSAnnotations bool `json:"useDNSAnnotations,omitempty"`
+
+	// UseCertificateAnnotations is a flag that can be used to add Certificate annotations to Certificate resources.
+	// This is only used when LoadBalancer.Spec.Hostname is set.
+	// +optional
+	UseCertificateAnnotations bool `json:"useCertificateAnnotations,omitempty"`
 }
 
 // EnvoyProxy defines the desired state of the EnvoyProxy
